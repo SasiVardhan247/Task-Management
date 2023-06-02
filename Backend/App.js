da3c62f13ea32ef23058a2a7802620993b0e9e4a
Sasi Vardhan A <83742554+SasiVardhan247@users.noreply.github.com>
2023-06-02 23:32:12 +0530

    Update App.js

--- Backend/App.js
+++ Backend/App.js
@@ -16,15 +16,12 @@ app.use(errorHandler);
 
 connectDb();
 
-// app.use('/api/task',taskRoute);
+app.use('/api/task',taskRoute);
 
 // if (process.env.NODE_ENV === "production") {
 //   app.use(express.static(path.resolve(__dirname, "../frontend/build")));
 //   app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "../frontend/build/index.html")));
 // }
-app.get('/about', (req, res) => {
-  res.send('This is my about route..... ')
-})
 
 app.listen(port, () => {
   console.log(`Backend is running on port ${port}`);
